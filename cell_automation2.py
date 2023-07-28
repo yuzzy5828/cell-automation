9c2dbecf1648589e88264ee97f95e576d1250f89
yuzzy5828 <130424862+yuzzy5828@users.noreply.github.com>
2023-07-29 00:58:31 +0900

    Update cell_automation2.py

--- cell_automation2.py
+++ cell_automation2.py
@@ -2,19 +2,41 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def apply_rule3(l_nb, current_num, r_nb):
-    # rule 184 をセットアップ(00011101)
-    if l_nb == 0 and current_num == 0 and r_nb == 0:
+# cn = current_number
+def apply_rule5(l2, l1, cn, r1, r2):
+    # fastcase をセットアップ(11011100110011111100110011000000)
+    if l2 == 0 and l1 == 0 and cn == 0 and r1 == 0 and r2 == 0: # 00000
         return 0
-    elif l_nb == 0 and current_num == 0 and r_nb == 1:
+    elif l2 == 0 and l1 == 0 and cn == 0 and r1 == 0 and r2 == 1: # 00001
         return 0
-    elif l_nb == 0 and current_num == 1 and r_nb == 0:
+    elif l2 == 0 and l1 == 0 and cn == 0 and r1 == 1 and r2 == 0: # 00010
         return 0
-    elif l_nb == 1 and current_num == 1 and r_nb == 0:
+    elif l2 == 0 and l1 == 0 and cn == 0 and r1 == 1 and r2 == 1: # 00011
+        return 0
+    elif l2 == 0 and l1 == 0 and cn == 1 and r1 == 0 and r2 == 0: # 00100
+        return 0
+    elif l2 == 0 and l1 == 0 and cn == 1 and r1 == 0 and r2 == 1: # 00101
+        return 0
+    elif l2 == 0 and l1 == 1 and cn == 0 and r1 == 0 and r2 == 0: # 01000
+        return 0
+    elif l2 == 0 and l1 == 1 and cn == 0 and r1 == 0 and r2 == 1: # 01001
+        return 0
+    elif l2 == 0 and l1 == 1 and cn == 1 and r1 == 0 and r2 == 0: # 01100
+        return 0
+    elif l2 == 0 and l1 == 1 and cn == 1 and r1 == 0 and r2 == 1: # 01101
+        return 0
+    elif l2 == 1 and l1 == 0 and cn == 1 and r1 == 0 and r2 == 0: # 10100
+        return 0
+    elif l2 == 1 and l1 == 0 and cn == 1 and r1 == 0 and r2 == 1: # 10101
+        return 0
+    elif l2 == 1 and l1 == 1 and cn == 0 and r1 == 0 and r2 == 0: # 11000
+        return 0
+    elif l2 == 1 and l1 == 1 and cn == 0 and r1 == 0 and r2 == 1: # 11001
+        return 0
+    elif l2 == 1 and l1 == 1 and cn == 1 and r1 == 0 and r2 == 1: # 11101
         return 0
     else:
         return 1
-
 def cellular_automation(initial_state, steps):
     # 初期状態をセットアップ
     current_state = initial_state[:]
@@ -27,9 +49,11 @@ def cellular_automation(initial_state, steps):
         # 新しい状態を計算
         for i in range(num_cells):
             # 周期境界条件を適用
-            l_nb = current_state[i - 1]
-            r_nb = current_state[(i + 1) % num_cells]
-            new_state[i] = apply_rule3(l_nb, current_state[i], r_nb)
+            l2 = current_state[i - 2]
+            l1 = current_state[i - 1]
+            r1 = current_state[(i + 1) % num_cells]
+            r2 = current_state[(i + 2) % num_cells]
+            new_state[i] = apply_rule5(l2, l1, current_state[i],r1, r2)
 
         # 状態を更新
         current_state = new_state[:]
@@ -38,7 +62,7 @@ def cellular_automation(initial_state, steps):
 
 if __name__ == "__main__":
     # 初期状態をランダムに設定
-    np.random.seed(357)  # シードを参照して再現性を持たせた
+    np.random.seed(480)  # シードを参照して再現性を持たせた
     initial_state = np.random.choice([0, 1], size=20)
 
     # シミュレーションのステップ数を指定
